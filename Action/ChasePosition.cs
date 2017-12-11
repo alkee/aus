411d15d8f8c3ac07adff7f7c918cb69f5f32bf0c
alkee <alkee@allm.co.kr>
2017-12-11 17:45:22 +0900

    support rigidbody movement for chasing

--- Action/ChasePosition.cs
+++ Action/ChasePosition.cs
@@ -15,19 +15,49 @@ namespace aus.Action
             ChasingTarget = target;
         }
 
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        void OnDisable()
+        {
+            if (rb != null)
+            {
+                rb.velocity = Vector3.zero; // stop
+            }
+        }
+
         void Update()
         {
-            if (ChasingTarget == null) return;
+            if (ChasingTarget == null || rb != null) return;
 
+            var direction = GetNormalizedTargetDirection();
+
+            var dt = Time.deltaTime;
+            transform.position += direction * dt * ChasingSpeed;
+        }
+
+        void FixedUpdate()
+        {
+            if (ChasingTarget == null || rb == null) return;
+
+            var direction = GetNormalizedTargetDirection();
+            rb.velocity = direction * ChasingSpeed;
+        }
+
+        private Rigidbody rb = null;
+
+        private Vector3 GetNormalizedTargetDirection()
+        {
+            if (ChasingTarget == null) return Vector3.zero;
             var direction = ChasingTarget.position - transform.position;
             if (FreezeAxis.X) direction.x = 0.0f;
             if (FreezeAxis.Y) direction.y = 0.0f;
             if (FreezeAxis.Z) direction.z = 0.0f;
-            if (direction == Vector3.zero) return;
+            if (direction == Vector3.zero) return Vector3.zero;
             direction.Normalize();
-
-            var dt = Time.deltaTime;
-            transform.position += direction * dt * ChasingSpeed;
+            return direction;
         }
     }
 }
