3d4ad9c4d1688b908747971989067133af2509dc
alkee <alkee@allm.co.kr>
2019-01-11 18:07:45 +0900

    add SetAsActiveScene property in InterScene.Dependency

--- InterScene/Dependency.cs
+++ InterScene/Dependency.cs
@@ -8,9 +8,12 @@ namespace aus.InterScene
     {
         [Tooltip("loads additive scene automatically")]
         public Property.SceneField DependentScene;
+        public bool SetAsActiveScene;
 
         void Awake()
         {
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
             if (enabled == false) return;
             Debug.Assert(DependentScene != null && string.IsNullOrEmpty(DependentScene.SceneName) == false
                 , "Dependent scene is not set");
@@ -26,6 +29,36 @@ namespace aus.InterScene
 
             // TODO: asyncronous load option
             SceneManager.LoadScene(DependentScene.SceneName, LoadSceneMode.Additive);
+        }
+
+        private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (SetAsActiveScene && scene.name == DependentScene.SceneName)
+            {
+                SceneManager.SetActiveScene(scene);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        private void OnValidate()
+        {
+            if (SetAsActiveScene)
+            {
+                // Active scene 은 하나밖에 있을 수 없으므로 모든 Dependency 를 확인해 수정
+                var deps = FindObjectsOfType<Dependency>();
+                foreach (var d in deps)
+                {
+                    if (d != this && d.SetAsActiveScene)
+                    {
+                        Debug.LogWarning(d.DependentScene.SceneName + " has marked out from SetAsActiveScene");
+                        d.SetAsActiveScene = false;
+                    }
+                }
+            }
         }
 
         // just to use 'enabled' in inspector
